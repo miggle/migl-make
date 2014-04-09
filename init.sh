57639bf4f45740e0488876da3e6b93624443d3ee
Steve <steve@miggle.co.uk>
2014-04-09 14:40:28 +0100

    adding init.sh script

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# create a new project using git@github.com:sgrichards/migl-make.git
+
+# open terminal tools/terminal
+
+# install box-parts
+parts install php5 php5-apache2 php5-pdo-mysql php5-gd mysql php5-curl composer;
+composer global require drush/drush:6.*;
+source ~/.bash_profile;
+
+# start services
+parts start apache2 mysql;
+
+# download Drupal, contrib and libraries
+drush make default.make -y;
+
+# manually add a database of your site
+# mysql -u root -p
+# CREATE DATABASE drupal;
+
